272c22b7717cd6d52d9d54e80146cb1cc6694179
campossalex <32500181+campossalex@users.noreply.github.com>
2025-11-01 18:36:53 +0100

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -4,11 +4,11 @@
 
 # Install kubctl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 kubectl /bin/kubectl
 
 # Install minikube
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo install minikube-linux-amd64 /bin/minikube
 
 # Install helm chart
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
