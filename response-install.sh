b6b47aef30a66c2b6ad8ebecfc5be710c85c862c
campossalex <32500181+campossalex@users.noreply.github.com>
2025-11-02 01:06:29 +0100

    Update response-install.sh

--- response-install.sh
+++ response-install.sh
@@ -3,7 +3,7 @@
 PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
 echo "Environment urls:\n"
-echo "VVP: http://$IP:8080"
-echo "Redpanda: http://$IP:8085"
-echo "Grafana: http://$IP:9090"
-echo "Web CLI: http://$IP:4200"
+echo "VVP: http://$PUBLIC_IP:8080"
+echo "Redpanda: http://$PUBLIC_IP:8085"
+echo "Grafana: http://$PUBLIC_IP:9090"
+echo "Web CLI: http://$PUBLIC_IP:4200"
