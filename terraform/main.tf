10c0844852700d5886a9d956fe7c4c9dcb55e277
campossalex <32500181+campossalex@users.noreply.github.com>
2025-11-30 21:58:15 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,7 +65,7 @@ resource "aws_instance" "registration_form" {
   ami           = "ami-004e960cde33f9146"
   instance_type = "t2.micro"
   key_name      = var.key_name
-  vpc_security_group_ids = [aws_security_group.ladday_sgn.id]
+  vpc_security_group_ids = [aws_security_group.labday_sgn.id]
 
   root_block_device {
     volume_size = 20          # GB
@@ -98,7 +98,7 @@ resource "aws_instance" "labday" {
   ami           = var.instance_ami
   instance_type = var.instance_type
   key_name      = var.key_name
-  vpc_security_group_ids = [aws_security_group.ladday_sgn.id]
+  vpc_security_group_ids = [aws_security_group.labday_sgn.id]
 
   root_block_device {
     volume_size = 40          # GB
